c67ff3e4d407ee70451bccfaec0f0a236e3c438a
devBeginner <kyeosu@naver.com>
2023-08-13 16:57:09 +0900

    0813-1

---
+++ Tip.js
@@ -0,0 +1,17 @@
+// 개발환경 설정: https://www.youtube.com/watch?v=Hm2DvKtDsgs
+
+/* 윈도우 cmd명령어
+  dir(보유 파일)
+  cd(이동)
+  mkdir(폴더 새로만들기)
+  Tab키 활용
+  code .(vs실행)
+
+  html유형 > html:5(기본세팅-link는 별도)
+
+*/
+
+
+// 포트폴리오 도메인: https://www.youtube.com/watch?v=WJtetccrv3o
+
+netlify;
